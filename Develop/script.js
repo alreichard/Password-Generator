ce37e16bb5063e4ba373e555b226009fa4dc349b
alreicha <areichard@haverford.edu>
2020-06-08 22:33:10 -0400

    Connected js to Html and added ReadMe

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,98 @@
-var i = 0
+//Needed variables:
+var i = 0;
 var specify;
 var Password = "";
+//Needed arrays:
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characters = ['@', '%', '+', "/","\\", "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
-var options = [];
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var characters = ['@', '%', '+', "/","o", "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
-
-// Assignment Code
+// Assignment Code:
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input:
 function writePassword() {
-  var password = generatePassword();
+  var password = passWordGen();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
-}
+};
 
-// Add event listener to generate button
+// Add event listener to generate button:
 generateBtn.addEventListener("click", writePassword);
 
+//Function that creates a random number between 0 and any given number (including 0):
 function random(number) {
   return Math.floor(Math.random() * number);
-}
+};
 
 
-// alert (lenght) = var lenof
-while (i == 0) {
+// while loop continues as long as certain contidtion is met:
+function passWordGen() {
+  var options = [];
+  while (i == 0) {
+  //prompts user for a length:
   var howmany = prompt("How long of a passwork would you like? (between 8 and 128 characters");
+  //sets peramiter on answers (if not an accepted answer, loops back through):
   if (8 > (parseInt(howmany)) ||(parseInt(howmany)) > 128) {
     alert("Length not in range. Please select a number between 8 and 128.");
   } else if (howmany == null) {
     alert("please select a number");
+  //peramiter is met, so while loop condition is broken (i!=0):
   } else {
     i = 1;
-    alert("yes");
+ 
   }
 }
-i=0
-// alert (lowercase) = var
-while (i == 0) {
-  var doYouWant = confirm("Would you like letters?");
-  if (doYouWant != null) {
+
+// while loop peramiter (i==1 now):
+while (i == 1) {
+  
+  var options = []
+  //creates 5 if statements. if each is true, it adds chosen attribute option to array, and breaks while loop condition:
+  var doYouWant = confirm("Would you like lowercase letters?");
+  if (doYouWant == true) {
+    //concat adds the chosen attribute to the "option" array:
+    //lowercase letter:
     options = options.concat("Lowercase");
     i++;
-    // for loop
+
   }
-  // alert (uppercase)
+  // uppercase letters:
   doYouWant = confirm("Would you like any letters to be uppercase?");
-  if (doYouWant != null) {
+  if (doYouWant == true) {
     options = options.concat("uppercase");
     i++;
   }
-  // alert number (number)
+  // numbers:
   doYouWant = confirm("Would you like numbers?");
-  if (doYouWant != null) {
+  if (doYouWant == true) {
     options = options.concat("number");
     i++;
+    
   }
 
-  // alert special 
+  //special characters:
   doYouWant = confirm("Would you like special characters?");
-  if (doYouWant != null) {
+  if (doYouWant == true) {
     options = options.concat("character");
     i++;
   }
-  else {
+  //if no options above are true, reloop:
+  if (i==1) {
     alert("you must choose atleast one character type.")
   }
 }
-
+// for stament that loops as many time as user password length input:
 for (i = 0; i < parseInt(howmany); i++) {
+  // for each loop through, variable "choose" equals one of the chosen attributes for the
+  //password from above. It does so based on their index that has been randomized:
   var choose = options[random(options.length)]
-
+ // the following if statements will choose a random character in the chosen array based on randomly chosen attribute from above:
   if (choose == "Lowercase" || choose == "uppercase") {
     specify = alphabet[random(26)];
-    console.log(specify);
+   // sub-category of letters, if the letter is upper or lower case, a random number will be chosen, if it is then uppercase, it 
+   //will be switched to one:
     if (choose == "uppercase") {
       specify = specify.toUpperCase();
     }
@@ -88,8 +103,10 @@ for (i = 0; i < parseInt(howmany); i++) {
   else if (choose == "character") {
     specify = characters[random(23)];
   }
+  //adds the character to the string of characters that will be teh final password
   Password = Password + specify
 }
+return Password
 
-alert(Password)
+};
 
--- README.md
+++ README.md
@@ -1,49 +1,27 @@
-# 03 JavaScript: Password Generator
+# Password Generator
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+This project allows for the user to choose from a list of password criteria: length, uppercase letter, lowercase letter, numbers, and characters. After they choose what they want, a random password, fitting tht criteria, will be generated. 
 
-If the user selects to include special characters in the password, here are the special characters you should select from:
-'@', '%', '+', '\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+The point of this project was for me to be able to explore different statements and loops including if statements, for loops and while loops. Through this project I was able to grasp all three. It also introduced me to the techniques required to connect HTML files to js files. 
 
-## User Story
+The most fun part about this was figuring out how to restart incase certain requirement weren't met (wasnt long enough or no characters selected). The while loop with a condition that could be fufilled or not worked well for this. 
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+# Installation 
 
-## Acceptance Criteria
+click on this link to see the cite:
+https://github.com/alreichard/Password-Generator
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
 
-The following image demonstrates the application functionality:
+# Usage
 
-![password generator demo](./Assets/03-javascript-homework-demo.png)
+First click on the red "Generate Password" button 
 
-## Review
+![](/pictures/picture1.png)
 
-You are required to submit the following for review:
+Then choose the password criteria
 
-* The URL of the deployed application.
+afters criteria are picked, the password will show
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+![](/pictures/picture2.png)
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
